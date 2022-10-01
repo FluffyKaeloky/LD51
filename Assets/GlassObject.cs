2b77ec4c475bfe6305bbee09c480e28d9e9ce77c
pierreJuzdzewski <pierrejuzdzewski@gmail.com>
2022-10-01 22:23:19 +0400

    Glass Break system Updated
    + glass switch instead of die

--- Assets/GlassObject.cs
+++ Assets/GlassObject.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent (typeof(Collider))]
 public class GlassObject : MonoBehaviour
 {
     [SerializeField] private GameObject baseModel, BeakedModel;
 
+    private Collider myCollider;
     public UnityEvent onGlassBreak = new UnityEvent();
+
+    private void Start()
+    {
+        baseModel.SetActive(true);
+        BeakedModel.SetActive(false);
+        myCollider = GetComponent<Collider>();
+    }
 
     public void BreakGlass()
     {
         baseModel.SetActive(false);
-        BeakedModel.SetActive(false);
+        BeakedModel.SetActive(true);
+        myCollider.enabled = false;
 
         onGlassBreak?.Invoke();
     }
