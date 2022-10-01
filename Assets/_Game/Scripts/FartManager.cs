881f5ed86ce5e3f1a6b1a0f901b08496168a36fb
pierreJuzdzewski <pierrejuzdzewski@gmail.com>
2022-10-01 13:00:42 +0400

    Fart system V2
    + Gradient to slider
    + Shake to slider

--- Assets/_Game/Scripts/FartManager.cs
+++ Assets/_Game/Scripts/FartManager.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class FartManager : MonoBehaviour
 {
     [SerializeField] private Slider fartSlider;
+    [SerializeField] private Image sliderFillImage;
+    [SerializeField] private Color lerpedColor, startColor, endColor;
 
     [SerializeField] private int refrainFartValue;
 
     private float loadFartSpeed;
     private float fartWillValue;
+
     [SerializeField] private float willValueSpeed;
     [SerializeField] private AnimationCurve fartWillCurve;
 
@@ -19,11 +23,14 @@ public class FartManager : MonoBehaviour
     private float noFartTime;
 
     public UnityEvent onFart = new UnityEvent();
+    public UnityEvent onHoldIt = new UnityEvent();
+
 
 
     private void Start()
     {
-        noFartTime = Time.time + 10;
+        sliderFillImage.color = startColor;
+        noFartTime = Time.time + 5;
     }
 
 
@@ -33,6 +40,8 @@ public class FartManager : MonoBehaviour
         if (noFartTime <= Time.time)
         {
             fartWillValue += Time.deltaTime * willValueSpeed;
+            lerpedColor = Color.Lerp(startColor, endColor, fartWillValue);
+            sliderFillImage.color = lerpedColor;
         }
 
 
@@ -48,19 +57,26 @@ public class FartManager : MonoBehaviour
     public void RefrainFart()
     {
         fartSlider.value -= refrainFartValue;
+        onHoldIt?.Invoke();
     }
 
 
     public void Fart()
     {
         print("farted");
+        fartSlider.gameObject.transform.DOShakePosition(0.8f,new Vector3(1,1,0),50,90,false, true,ShakeRandomnessMode.Full);
+        fartSlider.gameObject.transform.DOShakeScale(0.6f, new Vector3(0.1f, 0.1f, 0), 20, 90, true, ShakeRandomnessMode.Full);
+
         onFart?.Invoke();
 
         //resetFart
         fartSlider.value = 0;
         fartWillValue = 0;
         loadFartSpeed = 1;
-        noFartTime = Time.time + 10;
+        noFartTime = Time.time + 5;
+
+        lerpedColor = startColor;
+        sliderFillImage.color = startColor;
     }
 
 
